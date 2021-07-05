59c1ead786d387c721c08eb5800e8b78484e894b
Felipe Macário <felipesmacario@gmail.com>
2021-07-05 16:24:31 -0300

    CourseAngular

---
+++ Projeto-Angular/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([
+     
+      {
+        path: "", redirectTo: "courses",pathMatch: "full"
+      }      
+    ])
+  ], 
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
